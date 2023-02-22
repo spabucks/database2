7f9ae928748b69f8dae98a4d22dc530780d9d150
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-02-22 21:00:02 +0900

    소분류 카테고리 모델 작성

--- src/main/java/starbucks/smallcategory/model/SmallCategory.java
+++ src/main/java/starbucks/smallcategory/model/SmallCategory.java
@@ -1,4 +1,23 @@
 package starbucks.smallcategory.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import starbucks.bigcategory.model.BigCategory;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class SmallCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String smallCategory;
 }

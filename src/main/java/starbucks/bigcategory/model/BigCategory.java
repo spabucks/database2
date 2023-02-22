e5f2b0eb050396fa36c5897d20c536dfeff49e02
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-02-22 20:52:09 +0900

    대분류 카테고리 모델 작성

--- src/main/java/starbucks/bigcategory/model/BigCategory.java
+++ src/main/java/starbucks/bigcategory/model/BigCategory.java
@@ -1,11 +1,22 @@
 package starbucks.bigcategory.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class BigCategory {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String big;
+    private String bigCategory;
 }
